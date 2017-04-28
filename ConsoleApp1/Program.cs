98b999d4fa4b12d1f117c2162d95249a513249da
xxxhalo4lifexxx <xxxhalo4lifexxx@gmail.com>
2017-04-27 22:55:32 -0400

    stgsrtrth
    srthsrthr

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        int age = 16;
+        int airth = 6;
+        string name = "Franny";
+        int length = 6;
+        static bool degeneracy = true;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A lone Klansman gallops in on his horse, reaching an abrupt stop when he sees you");
+            Console.WriteLine("\"I Hate Fucking Niggers\" the elegant white robed warrior says");
+            Console.WriteLine("\"Whats your name whiteboy\"");
+
+
+
+            string input = Console.ReadLine();
+            switch (input)
+
+            {
+                default:
+                    degeneracy = false;
+                    break;
+                case "Emil":
+                    degeneracy = true;
+                    break;
+                case "Kyle":
+                    degeneracy = true;
+                    break;
+                case "Franny":
+                    degeneracy = true;
+                    break;
+                case "Chris":
+                    degeneracy = true;
+                    break;
+                case "Nigger":
+                    degeneracy = true;
+                    break;
+
+            }
+
+            if (degeneracy == true)
+
+            {
+                Console.WriteLine("Faggot Coalburner Niggerlover");
+            }
+           
+            else
+            {
+                Console.WriteLine("\"Welcome Brother\"");
+                Console.WriteLine(" He hands you a shiny gladius");
+                Console.WriteLine("\"Paint it red for me\"");
+                Console.WriteLine("He pulls you onto his brilliant white stallion and guides it towards the NAACP center");
+                
+                Weapon new Weapon(true, "gladius", 0, 2, 70, 2);
+                Colosole.WriteLine("")
+
+
+            }
+ Console.ReadLine();
+        } 
+    }
+}
---
+++ ConsoleApp1/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Weapon
+    {
+        bool melee;
+        string name;
+        int ammo;
+        int damage;
+        int accuracy;
+        int burst;
+        public Weapon(bool isMelee,string whatName,int whatAmmo,int whatDamage,int whatAccuracy,int whatburst)
+        {
+            melee = isMelee;
+            name = whatName;
+            ammo = whatAmmo;
+            damage = whatDamage;
+            accuracy = whatAccuracy;
+            burst = whatburst;
+
+        }
+    }
+}
